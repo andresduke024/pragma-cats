a0a4415ed12289c93eceac852b6e1acda80fd400
Andres Duque <andresduke024@gmail.com>
2022-12-05 23:06:43 -0500

    Initial Commit

---
+++ pragma-cats/pragma_catsApp.swift
@@ -0,0 +1,17 @@
+//
+//  pragma_catsApp.swift
+//  pragma-cats
+//
+//  Created by Andres Duque on 5/12/22.
+//
+
+import SwiftUI
+
+@main
+struct pragma_catsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
